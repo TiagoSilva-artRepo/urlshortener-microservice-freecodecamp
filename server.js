cac0a7c2c2abef43b9d68b5586f7ff2908063cec
Tiago Silva <tiago.silva@contisystems.pt>
2021-11-12 12:38:49 +0000

    fix response

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+var bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -14,16 +15,19 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 var urlsData = [];
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  //urlsData.push(req.body.url);
-  res.json({ original_url: "hello" });
+  urlsData.push(req.body.url);
+  res.json({ original_url: req.body.url, short_url: urlsData.findIndex((url)=>url === req.body.url) + 1 });
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
-
-//short_url: urlsData.findIndex((url)=>url === req.body.url) + 1 }
