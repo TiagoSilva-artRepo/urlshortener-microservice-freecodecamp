5f1fcc0be32913839c8e7287708a0e5c448833e1
Tiago Silva <tiago.silva@contisystems.pt>
2021-11-12 14:34:50 +0000

    fix response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var urlsData = [];
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  var expression = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)";
+  var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
   var regex = new RegExp(expression);
 
   if (!req.body.url.match(regex)) {
